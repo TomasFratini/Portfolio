0abb4fe7071501c80b77270c992566eeab4a3dd9
Tomas Fratini <tomas.fratini09@gmail.com>
2022-07-19 23:26:37 -0300

    portfolio final

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,10 +5,10 @@ return (
 
   <div class="navbar bg-base-100">
     <div class="flex-1">
-      <a href="/" class="btn btn-ghost normal-case text-xl tipografia">Tomas Fratini</a>
+      <a href="/" class="btn btn-ghost normal-case text-xl texto">tf</a>
     </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal p-0">
+    <div class="flex-none ">
+      <ul class="menu menu-horizontal p-0  ">
         <li><a className="btn btn-ghost normal-case text-lg innert nav" href="/sobremi">Sobre mi</a></li>
         <li><a className="btn btn-ghost normal-case text-lg innert nav" href="/contacto">Contacto</a></li>
         <li><a className="normal-case text-lg linkedin" target="blank" href="https://www.linkedin.com/in/tomas-fratini/">@FratiniTomas</a></li>
--- src/components/sobremi/Sobremi.js
+++ src/components/sobremi/Sobremi.js
@@ -11,7 +11,7 @@ const Sobremi = () => {
         <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
             <h1 className="mb-5 text-3xl font-bold nav pb-52">Desarrollador de software</h1>
-            <p className="mb-5 items text-justify   ">Hi! mi nomre es Tomas y soy desarrollador de software orientado a la rama del Front end.
+            <p className="mb-5 items text-justify   ">Hi! mi nomre es Tomas Fratini y soy desarrollador de software orientado a la rama del Front end.
             He realizado la carrera de desarrollador front end en CoderHouse; en el mismo realice el curso de Desarrollo Web, JavaScript y React.
             Me encuentro sumamente motivado por aprender nuevas tecnologias y herramientas que me aporten experiencia y conocimiento en el mundo del desarrollo.
             </p>
