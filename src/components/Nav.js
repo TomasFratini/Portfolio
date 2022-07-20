8e81e4519055b044fab0bbb6510cc29d91661ee1
Tomas Fratini <tomas.fratini09@gmail.com>
2022-07-19 23:19:46 -0300

    Ultimos arreglos

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,23 +2,19 @@ function Nav() {
 
 
 return (
-<div className="navbar flex justify-end fondo text-black pt-5 pr-5 ">
-  <li class="flex-1">
-    <a  href="/" class="btn btn-ghost normal-case text-xl tipografia">Tomas Fratini</a>
-  </li>
-    <li className="btn btn-ghost normal-case text-lg" tabIndex="0">
-      <a href="/" className="innert nav">Proyectos</a>
-    </li>
-    <li className="btn btn-ghost normal-case text-lg" tabIndex="0">
-      <a href="/sobremi" className="innert nav">Sobre mi</a>
-    </li>
-    <ul className="btn btn-ghost normal-case text-lg ">
-      <li><a href="/contacto" className="innert nav">Contacto</a></li>
-    </ul>
-    <ul className="linkedin normal-case text-lg linkedin ">
-      <li><a target="blank" href="https://www.linkedin.com/in/tomas-fratini/">@FratiniTomas</a></li>
-    </ul>
-</div>
+
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a href="/" class="btn btn-ghost normal-case text-xl tipografia">Tomas Fratini</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal p-0">
+        <li><a className="btn btn-ghost normal-case text-lg innert nav" href="/sobremi">Sobre mi</a></li>
+        <li><a className="btn btn-ghost normal-case text-lg innert nav" href="/contacto">Contacto</a></li>
+        <li><a className="normal-case text-lg linkedin" target="blank" href="https://www.linkedin.com/in/tomas-fratini/">@FratiniTomas</a></li>
+      </ul>
+    </div>
+  </div>
   )
 }
 export default Nav
