ec3922aebcd7b69c9aec9902571de3ed920f2a5c
Tomas Fratini <tomas.fratini09@gmail.com>
2023-07-22 21:08:13 +0200

    fix navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
   const [t, i18n] = useTranslation("global");
 
   return (
-    <div className="navbar bg-base-100">
+  <div className="navbar bg-base-100 navbar-min">
     <div className="flex-1">
       <a href="/" className="btn btn-ghost normal-case text-xl texto">tf</a>
     </div>
@@ -16,9 +16,9 @@ const Nav = () => {
         <li><a className="btn btn-ghost normal-case text-lg innert nav" href="/sobremi">{t('nav.about')}</a></li>
         <li><a className="btn btn-ghost normal-case text-lg innert nav" href="/contacto">{t('nav.contact')}</a></li>
         <li><a className="normal-case text-lg linkedin" target="blank" href="https://www.linkedin.com/in/tomas-fratini/">@FratiniTomas</a></li>
+      </ul>
         <button onClick={() => i18n.changeLanguage("es")}><img  alt='icono' width='30px' src={español}/></button>
         <button onClick={() => i18n.changeLanguage("en")}><img  alt='icono' width='25px' src={ingles}/></button>
-      </ul>
     </div>
   </div>
   )
--- src/components/presentacion/Proyectos.js
+++ src/components/presentacion/Proyectos.js
@@ -9,7 +9,7 @@ function Proyectos() {
   return (
   <>
   <div className="grid justify-items-stretch">
-    <div className="hero min-h-screen bg-base-200 max-w-7xl justify-self-end ">
+    <div className="hero min-h-screen bg-base-200  max-w-7xl justify-self-end ">
       <div className="hero-content flex-col lg:flex-row ">
         <img alt="img" src={perfil} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
